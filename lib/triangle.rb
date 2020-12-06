09b3874ce9f609dcc42af0838f23f28063a9c467
Vikram Chatterjee <strycora@gmail.com>
2020-12-05 17:19:21 -0700

    commit tree b7cdce6996cbd7d51aa88f52a03847771267cbf3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+    # def initialize(attributes)
+    #   attributes.each do |key, value|
+    #     self.class.attr_accessor(key)
+    #     self.send(("#{key}="), value)
+    #   end
+    # end
+
+    class TriangleError < StandardError
+      def message
+        "You must provide sides of a valid triangle"
+      end
+    end
+
+    def kind
+      if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) || (side1 <= 0 || side2 <=0 || side3 <= 0)
+        raise TriangleError
+      end
+
+      if side1 == side2 && side2 == side3
+        :equilateral
+      elsif (side1 == side2 && side2 != side3) || (side1 != side2 && side2 == side3) || (side1 == side3 && side1 != side2)
+        :isosceles
+      elsif side1 != side2 && side2 != side3 && side1 != side3
+        :scalene
+      end
+    end
 end
+
